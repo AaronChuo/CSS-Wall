1ab00c245181f319d25441eb8f122b368ad155f1
Aaron Chuo <crazychuo@gmail.com>
2014-08-06 14:53:51 +0800

    Build app structure and index page.

---
+++ js/controllers/kanban.controller.js
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------
+//
+//--------------------------------------------------------------
+'use strict';
+
+angular
+  .module('CSSKanban')
+  .controller('kanbanController', kanbanController);
+
+  kanbanController.$inject = [];
+
+  function kanbanController() {
+    var vm = this;
+
+    //-------------------------------------
+    // ViewModel Interface
+    //-------------------------------------
+
+    //-------------------------------------
+    // Implements
+    //-------------------------------------
+
+  }
